40d00ce4901704e7afcee59194beae1519f1d007
goulustis <cnimaiv@gmail.com>
2022-12-04 02:35:45 -0800

    add setup.py for setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+        name = "gracefall",
+        packages=find_packages(),
+        description="cpsc547 viz project",
+        author="Ian Hill, Matthew Tang",
+        version="0.0.0"
+)
