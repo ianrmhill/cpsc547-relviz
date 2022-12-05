fa427f7e21ec25ce3a6c4551d3718cf8fe56675f
goulustis <cnimaiv@gmail.com>
2022-12-04 23:53:33 -0800

    add in pca chart

--- gracefall/view_arrangers.py
+++ gracefall/view_arrangers.py
@@ -5,8 +5,8 @@ import altair
 __all__ = ['assemble_test_view', 'arrange_test_views']
 
 
-def assemble_test_view(time_plot, strs_plot, time_hint):
-    return time_hint & time_plot & strs_plot
+def assemble_test_view(time_plot, strs_plot, time_hint, pca_plot):
+    return time_hint & time_plot & strs_plot & pca_plot
 
 
 def arrange_test_views(test_view):
